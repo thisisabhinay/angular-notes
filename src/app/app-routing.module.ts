acefee0c4f04e0cc1bc17cca79f3c607e4a90353
Abhinay Thakur <a8h1nay@gmail.com>
2020-10-29 23:02:24 +0530

    Generates and route new component "Notes List"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "",
+        component: NotesListComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
