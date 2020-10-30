a62a4619ceb6a4371646d82ed037c94827737688
Abhinay Thakur <a8h1nay@gmail.com>
2020-10-30 11:33:03 +0530

    Generates Note Card component

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-note-card",
+    templateUrl: "./note-card.component.html",
+    styleUrls: ["./note-card.component.sass"]
+})
+export class NoteCardComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
