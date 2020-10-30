c0c8eb462f19584bd4ff09955ae6c6190dab787a
Abhinay Thakur <a8h1nay@gmail.com>
2020-10-30 11:32:33 +0530

    Create Main Layout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { NotesListComponent } from './pages/notes-list/notes-list.component';
+import { MainLayoutComponent } from "./pages/main-layout/main-layout.component";
+import { NotesListComponent } from "./pages/notes-list/notes-list.component";
 
 const routes: Routes = [
     {
         path: "",
-        component: NotesListComponent
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: "",
+                component: NotesListComponent
+            }
+        ]
     }
 ];
 
