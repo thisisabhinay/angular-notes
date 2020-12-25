e567750962bde7f3faf9ea5ef70431c15237fa46
Abhinay Thakur <a8h1nay@gmail.com>
2020-12-25 17:09:15 +0530

    Using Notes service to create new note

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -2,6 +2,7 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Note } from "src/app/shared/note.model";
+import { NotesService } from "src/app/shared/notes.service";
 
 @Component({
     selector: "app-note-details",
@@ -11,14 +12,13 @@ import { Note } from "src/app/shared/note.model";
 export class NoteDetailsComponent implements OnInit {
     note: Note;
 
-    constructor() { }
+    constructor(private notesService: NotesService) { }
 
     ngOnInit(): void {
         this.note = new Note();
     }
 
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    onSubmit(form: NgForm) {
-        console.log(form);
+    onSubmit(form: NgForm): void {
+        this.notesService.add(form.value);
     }
 }
